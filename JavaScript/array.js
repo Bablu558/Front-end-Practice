0f2b4bea2fd05daa408db5f4caf846ce550cb121
Git-Github <kumarbablu03240@gmail.com>
2025-10-10 12:52:14 +0530

    latest

---
+++ JavaScript/array.js
@@ -0,0 +1,34 @@
+let color = ["pink","yellow","green","red"]
+console.log(color);
+console.log(color[1]);
+
+
+
+let odd =new  Array(1,3,5,7,2);
+console.log(odd);
+console.log(odd[1]);
+
+
+
+let even = odd.filter(num => num %2 ===0);
+console.log(even);
+
+
+// let info =[1,3,5,7,"Nidhi"];
+// console.log(info);
+// console.log(info[1]);
+
+
+// color.push("orrange");
+// color.unshift("white");
+// console.log(color);
+
+// color.pop();
+// color.shift();
+// color.shift();
+// console.log(color);
+
+
+// let fruits = ["apple", "banana", "cherry"];
+// fruits.forEach(fruit => console.log(fruit));
+// fruits.forEach(fruit => Process.stdout.write(fruit + " "));
